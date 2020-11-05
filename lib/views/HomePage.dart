f017b8ffb4254bba4c5901a93d88d63a5f1bbf15
ThanhDang143 <ThanhDang143@users.noreply.github.com>
2020-11-05 11:58:28 +0700

    First commit. Create project, main UI.

---
+++ lib/views/HomePage.dart
@@ -0,0 +1,73 @@
+import 'package:calendar_app/widget/widget.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CalendarController _calendarController;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar("Thanhhh's Calendar"),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TableCalendar(
+              weekendDays: [DateTime.sunday],
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              initialCalendarFormat: CalendarFormat.month,
+              calendarStyle: CalendarStyle(
+                //todayColor: Colors.blueAccent,
+                //selectedColor: Theme.of(context).primaryColor.withOpacity(0.5),
+                // todayStyle: TextStyle(
+                //     fontSize: 20,
+                //     fontWeight: FontWeight.bold,
+                //     color: Colors.white),
+                // selectedStyle: TextStyle(
+                //     fontSize: 20,
+                //     fontWeight: FontWeight.bold,
+                //     color: Colors.white),
+                outsideStyle: TextStyle(
+                  color: Colors.black.withOpacity(0.2),
+                  fontSize: 12,
+                ),
+              ),
+              headerStyle: HeaderStyle(
+                formatButtonDecoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                formatButtonTextStyle: TextStyle(
+                  fontWeight: FontWeight.bold, color: Colors.white
+                )
+              ),
+              onDaySelected: (date, events,_) {
+                print(date.toString());
+              },
+              builders: CalendarBuilders(
+                selectedDayBuilder: (context, date, events) => 
+                  containerDecor(date, Colors.lightBlue, 50, Colors.white),
+                todayDayBuilder: (context, date, events) => 
+                  containerDecor(date, Colors.blue.withOpacity(0.25), 50, Colors.white),
+              ),
+              
+              calendarController: _calendarController,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+Widget appBar(String title) {
+  return AppBar(
+    title: Text(title),
+    centerTitle: true,
+  );
+}
+
+Widget containerDecor(DateTime date, Color boxColor, double borderRadius, Color textColor) {
+  return Container(
+    margin: EdgeInsets.all(10),
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+      color: boxColor,
+      //shape: boxShape,
+      borderRadius: BorderRadius.circular(borderRadius)
+    ),
+    child: Text(
+      date.day.toString(),
+      style: TextStyle(
+        color: textColor,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    )
+  );
+}
